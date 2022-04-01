e8f31355fe9333f1652d149ee3fcbf8fb3e541bf
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2022-04-01 04:26:03 +0200

    base html

--- GiveAway/urls.py
+++ GiveAway/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main_app.views import LandingPageView, AddDonationView, LoginView, RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name='main'),
+    path('add-donation/', AddDonationView.as_view(), name='add-donation'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
