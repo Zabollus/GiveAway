6991b5091a9b117854f53b19c705521c4c73df0b
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2022-04-16 11:40:52 +0200

    admin update

--- main_app/admin.py
+++ main_app/admin.py
@@ -4,15 +4,23 @@ from django.contrib.auth.models import User
 from main_app.models import Donation, Institution, Category
 
 
-def indexes_of_categories(obj):
+def names_of_categories(obj):
     result = []
     for category in obj.categories.all():
-        result.append(category.id)
+        result.append(category.name)
     return result
 
 
+def user_full_name(obj):
+    return obj.user.first_name + ' ' + obj.user.last_name
+
+
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ['institution', 'quantity', 'city', user_full_name, 'is_taken']
+
+
 class InstitutionAdmin(admin.ModelAdmin):
-    list_display = ['name', indexes_of_categories]
+    list_display = ['name', names_of_categories]
 
 
 class MyUserAdmin(UserAdmin):
@@ -26,6 +34,6 @@ class MyUserAdmin(UserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
-admin.site.register(Donation)
+admin.site.register(Donation, DonationAdmin)
 admin.site.register(Category)
 admin.site.register(Institution, InstitutionAdmin)
